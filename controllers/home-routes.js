5d5440a4f71c78cf8be6c531eb613ae78f399e6a
Chris Tolnai <christolnai@gmail.com>
2021-09-22 13:42:18 -0700

    first push with starter code

---
+++ controllers/home-routes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { User, Project } = require('../models');
+// Import the custom middleware
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: Project,
+          attributes: ['name', 'description'],
+        },
+      ],
+    });
+
+    const projects = projectData.map((project) =>
+      project.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      projects,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+// Use the custom middleware before allowing the user to access the gallery
+router.get('/gallery/:id', withAuth, async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            'id',
+            'title',
+            'artist',
+            'exhibition_date',
+            'filename',
+            'description',
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+// Use the custom middleware before allowing the user to access the painting
+router.get('/painting/:id', withAuth, async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
